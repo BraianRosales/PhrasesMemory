8dba453e6148e10864bd47c38f78d7a5dc2cc4e6
Braian Rosales <braianezequielrosales@gmail.com>
2024-05-29 01:09:02 -0300

    I added main project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'decks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'decks',
+    loadComponent: () =>
+      import('./modules/decks/decks.component').then((c) => c.DecksComponent),
+  },
+  {
+    path: 'config',
+    loadComponent: () =>
+      import('./modules/config/config.component').then((c) => c.ConfigComponent),
+  },
+];
