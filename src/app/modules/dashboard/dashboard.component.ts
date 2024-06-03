0d2aaa6cd0f32ed8da83f953346cbc05ae83823c
Braian Rosales <braianezequielrosales@gmail.com>
2024-06-03 18:52:07 -0300

    Improved the style design of the user interface + dashboard layout

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PhraseFinderComponent } from 'src/app/shared/components/phrase-finder/phrase-finder.component';
+import { LoginComponent } from 'src/app/auth/login/login.component';
+import { CardsBoardComponent } from 'src/app/shared/components/cards-board/cards-board.component';
+import { CardSearchHistoryComponent } from 'src/app/shared/components/card-search-history/card-search-history.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PhraseFinderComponent, LoginComponent, CardsBoardComponent, CardSearchHistoryComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GetAnAccountComponent } from '../get-an-account/get-an-account.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GetAnAccountComponent],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
